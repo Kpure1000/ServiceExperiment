ab2a60508a13f41adba008e4ed4fcc57bfaff486
Kpurek <ktsq2000@126.com>
2020-06-17 00:01:14 +0800

    update

--- network/serviceNetwork.java
+++ network/serviceNetwork.java
@@ -17,30 +17,30 @@ public class serviceNetwork {
      * 端口号
      */
     private int port = 12834;
-    
+
     /**
      * 线上最大用户数
      */
     final int maxUserNumber = 10;
 
-    private int curUserNumber=0;
+    private int curUserNumber = 0;
+
     public serviceNetwork() throws IOException {
         System.out.println("端口: " + port);
         serverSocket = new ServerSocket(port);
         try {
             while (true) {
                 var client = serverSocket.accept();
-                System.out.print(
-                        "用户 " + client.getInetAddress().toString() + ":" + client.getPort() + " 上线: " + client.toString());
+                System.out.print("用户 " + client.getInetAddress().toString() + ":" + client.getPort() + " 上线: "
+                        + client.toString());
                 new Thread(new serverThread(client)).start();
             }
         } catch (Exception e) {
             e.printStackTrace();
-        } finally{
+        } finally {
             // TODO 关掉资源
         }
 
-        
     }
 
 }
